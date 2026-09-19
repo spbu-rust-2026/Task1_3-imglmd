c114f550a9dc684f8e219f3e6c015c1a9d2e3886
kir <dangdatmini@gmail.com>
2026-09-19 09:44:44 +0300

    complete homework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::{fs, io};
+
 fn main() {
-    println!("Hello, world!");
+    let mut path = String::new();
+
+    if io::stdin().read_line(&mut path).is_err() {
+        println!("failure");
+        return;
+    }
+
+    if path.trim().is_empty() {
+        print!("failure");
+        return;
+    }
+    match fs::read_to_string(path) {
+        Ok(content) => content,
+        Err(_) => {
+            print!("failure");
+            return;
+        }
+    };
+
+    print!("success");
 }
